c1f83044a7b097309e6c946ebb16943b0e0d731a
bennett-abauer <61594943+bennett-abauer@users.noreply.github.com>
2020-05-06 14:14:05 -0500

    finished styling recruitment form

--- src/pages/recruitmentPage/recruitmentPage.component.jsx
+++ src/pages/recruitmentPage/recruitmentPage.component.jsx
@@ -1,14 +1,17 @@
 import React from "react";
 import RecruitmentForm from "../../components/recruitmentForm/recruitmentForm.component";
 
+import "./recruitmentPage.styles.scss";
+
 class RecruitmentPage extends React.Component {
     constructor() {
         super();
 
         this.state = {
-            lastName: "",
             firstName: "",
+            lastName: "",
             address: "",
+            addressLine2: "",
             phoneNumber: "",
             email: ""
         }
@@ -21,48 +24,67 @@ class RecruitmentPage extends React.Component {
     }
 
     render() {
-        const { lastName, firstName, address, phoneNumber, email } = this.state;
+        const { lastName, firstName, address, addressLine2, phoneNumber, email } = this.state;
         return (
             <div className="recruitment">
                 <h2 className="title">Doane Athletics Women's Basketball Recruitment Form</h2>
                 <span>Please fill out the form below to the best of your knowledge</span>
-                <form className="recruitment-form">
-                    <RecruitmentForm
-                        type="text"
-                        name="lastName"
-                        value={ lastName }
-                        onChange={ this.handleChange }
-                        label="Last Name"
-                        required />
-                    <RecruitmentForm
-                        type="text"
-                        name="firstName"
-                        value={ firstName }
-                        onChange={ this.handleChange }
-                        label="First Name"
-                        required />
-                    <RecruitmentForm
-                        type="text"
-                        name="address"
-                        value={ address }
-                        onChange={ this.handleChange }
-                        label="Address"
-                        required />
-                    <RecruitmentForm
-                        type="text"
-                        name="phoneNumber"
-                        value={ phoneNumber }
-                        onChange={ this.handleChange }
-                        label="Phone Number"
-                        required />
-                    <RecruitmentForm
-                        type="text"
-                        name="email"
-                        value={ email }
-                        onChange={ this.handleChange }
-                        label="Email"
-                        required />
-                </form>
+                <div className="grid-container">
+                    <form className="firstName">
+                        <RecruitmentForm
+                            type="text"
+                            name="firstName"
+                            value={ firstName }
+                            onChange={ this.handleChange }
+                            label="First Name"
+                            required />
+                    </form>
+                    <form className="lastName">
+                        <RecruitmentForm
+                            type="text"
+                            name="lastName"
+                            value={ lastName }
+                            onChange={ this.handleChange }
+                            label="Last Name"
+                            required />
+                    </form>
+                    <form className="address">
+                        <RecruitmentForm
+                            type="text"
+                            name="address"
+                            value={ address }
+                            onChange={ this.handleChange }
+                            label="Address"
+                            required />
+                    </form>
+                    <form className="addressLine2">
+                        <RecruitmentForm
+                            type="text"
+                            name="addressLine2"
+                            value={ addressLine2 }
+                            onChange={ this.handleChange }
+                            label="Address: Line 2"
+                            required />
+                    </form>
+                    <form className="phoneNumber">
+                        <RecruitmentForm
+                            type="text"
+                            name="phoneNumber"
+                            value={ phoneNumber }
+                            onChange={ this.handleChange }
+                            label="Phone Number"
+                            required />
+                    </form>
+                    <form className="email">
+                        <RecruitmentForm
+                            type="text"
+                            name="email"
+                            value={ email }
+                            onChange={ this.handleChange }
+                            label="Email"
+                            required />
+                    </form>
+                </div>
             </div>
         )
     }

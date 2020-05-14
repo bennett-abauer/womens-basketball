6054a042c73459b2bb7b1474da0f5832801a32f3
bennett-abauer <61594943+bennett-abauer@users.noreply.github.com>
2020-05-14 10:51:45 -0500

    added table to tabview of recruit namese and contact info, created flask backend server and python script to access database and return data to table

--- flask_backend/main.py
+++ flask_backend/main.py
@@ -1,5 +1,11 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
+import json
+import psycopg2 as pg2
+from psycopg2.extras import RealDictCursor
 import os
+
+CONN = pg2.connect(database="basketball-recruitment", user="postgres", password=9206)
+CUR = CONN.cursor(cursor_factory=RealDictCursor)
 
 app = Flask(__name__, template_folder="../build", static_folder="../build/static")
 
@@ -13,6 +19,15 @@ def catch_all(path):
     """ This is a catch all that is required for react-router """
     return render_template("index.html")
 
+@app.route("/viewRecruits", methods=["POST"])
+def viewRecruits():
+    query_sql = '''SELECT last_name, first_name, address, phone_number, email FROM recruits;'''
+    CUR.execute(query_sql)
+    results = CUR.fetchall()
+
+    return json.dumps(results)
+
 if __name__ == "__main__":
+#    viewRecruits()
     port = int(os.environ.get("PORT", 5000))
-    app.run(host="0.0.0.0", port=port)
+    app.run(host="127.0.0.1", port=port)
--- src/components/table-view/table-view-recruits.component.jsx
+++ src/components/table-view/table-view-recruits.component.jsx
@@ -1,34 +1,55 @@
 import React from "react";
-import TableView from "react-table-view";
+import axios from "axios";
 
 import "./table-view.styles.scss";
 
 class RecruitTable extends React.Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            recruitData: []
+        }
+    }
+
+    componentDidMount() {
+        axios
+        .post("/viewRecruits")
+        .then(response => response.data).then(data => {
+            this.setState({ recruitData: data })
+        })
+        .catch((error) => {
+            console.log(error);
+        })
+    }
+
+    componentDidUpdate() {
+        console.log(this.state.recruitData);
+    }
 
     render() {
-        const DATA = [
-            { id: 0, make: "Honda", model: "NSX", year: "1997" },
-            { id: 1, make: "Toyota", model: "Supra", year: "1996" },
-            { id: 2, make: "Nissan", model: "300ZX", year: "1998" }
-        ]
 
-        let COLUMNS = {
-            make: function(data) {
-                return <span>{ data.year }</span>
-            },
-            model: function(data) {
-                return <span>{ data.model }</span>
-            },
-            year: function(data) {
-                return <span>{ data.year }</span>
-            }
-        }
-
-        return (
-            <div>
-                <TableView data={ DATA } columns={ COLUMNS } />
-            </div>
-        )
+        return(
+            <table>
+                <tr>
+                    <th>Last Name</th>
+                    <th>First Name</th>
+                    <th>Address</th>
+                    <th>Phone Number</th>
+                    <th>Email</th>
+                </tr>
+                { this.state.recruitData.map(recruit => {
+                    return (
+                    <tr>
+                        <td>{ recruit.last_name }</td>
+                        <td>{ recruit.first_name }</td>
+                        <td>{ recruit.address }</td>
+                        <td>{ recruit.phone_number }</td>
+                        <td>{ recruit.email }</td>
+                    </tr>
+                )})}
+            </table>
+        );
     }
 }
 
